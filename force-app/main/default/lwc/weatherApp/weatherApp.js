923b3c550f681b3186f94acae2e96c8e6a909149
Matheus <mathugolini@gmail.com>
2023-07-25 19:41:25 -0300

    Adição de funcao searchHandler e submitHandler

--- force-app/main/default/lwc/weatherApp/weatherApp.js
+++ force-app/main/default/lwc/weatherApp/weatherApp.js
@@ -1,3 +1,18 @@
 import { LightningElement } from 'lwc';
 
-export default class WeatherApp extends LightningElement {}
+export default class WeatherApp extends LightningElement {
+    cityName = ''
+    searchHandler(event) {
+        this.cityName = event.target.value
+    }
+
+    submitHandler(event) {
+        event.preventDefault() 
+        this.fetchData()     
+    }
+
+    fetchData() {
+        console.log("cityName", this.cityName)
+        // chamada da api
+    }
+}
