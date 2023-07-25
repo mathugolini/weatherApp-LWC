9d8bacc2650ee5d83974cb80b3efed0c5c213eeb
Matheus <mathugolini@gmail.com>
2023-07-25 19:47:00 -0300

    Criação de constante para chave de api

--- force-app/main/default/lwc/weatherApp/weatherApp.js
+++ force-app/main/default/lwc/weatherApp/weatherApp.js
@@ -1,5 +1,6 @@
 import { LightningElement } from 'lwc';
-
+const API_KEY = '422336ee0e72ac783393c3393bd4a281'
+//  chave de api gerada pelo site https://home.openweathermap.org/
 export default class WeatherApp extends LightningElement {
     cityName = ''
     searchHandler(event) {
