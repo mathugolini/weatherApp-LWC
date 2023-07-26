e15b40965e1887b95e0869ab00eb2fec7ead0e04
Matheus <mathugolini@gmail.com>
2023-07-25 22:14:32 -0300

    Import de static resources

--- force-app/main/default/lwc/weatherApp/weatherApp.js
+++ force-app/main/default/lwc/weatherApp/weatherApp.js
@@ -2,7 +2,20 @@ import { LightningElement } from 'lwc';
 const API_KEY = '422336ee0e72ac783393c3393bd4a281'
 //  chave de api gerada pelo site https://home.openweathermap.org/
 
+import WEATHER_ICONS from 'salesforce/resourceURL/weatherAppIcons'
+
 export default class WeatherApp extends LightningElement {
+
+    clearIcon = WEATHER_ICONS + '/weatherIcons/clear.svg'
+    cloudIcon = WEATHER_ICONS + '/weatherIcons/cloud.svg'
+    dropletIcon = WEATHER_ICONS + '/weatherIcons/droplet.svg'
+    hazeIcon = WEATHER_ICONS + '/weatherIcons/haze.svg'
+    mapIcon = WEATHER_ICONS + '/weatherIcons/map.svg'
+    rainIcon = WEATHER_ICONS + '/weatherIcons/rain.svg'
+    snowIcon = WEATHER_ICONS + '/weatherIcons/snow.svg'
+    stormIcon = WEATHER_ICONS + '/weatherIcons/storm.svg'
+    thermometerIcon = WEATHER_ICONS + '/weatherIcons/thermometer.svg'
+    arrowBackIcon = WEATHER_ICONS + '/weatherIcons/arrow-back.svg'
 
     cityName = ''
     loadingText = ''
