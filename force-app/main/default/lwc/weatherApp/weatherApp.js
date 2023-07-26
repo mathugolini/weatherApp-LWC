3f294e22ca34d490405d07d46073f909388e2b85
Matheus <mathugolini@gmail.com>
2023-07-25 21:23:12 -0300

    implementação de regra de negocio para retorno de mensagem com sucesso ou sem sucesso

--- force-app/main/default/lwc/weatherApp/weatherApp.js
+++ force-app/main/default/lwc/weatherApp/weatherApp.js
@@ -1,8 +1,17 @@
 import { LightningElement } from 'lwc';
 const API_KEY = '422336ee0e72ac783393c3393bd4a281'
 //  chave de api gerada pelo site https://home.openweathermap.org/
+
 export default class WeatherApp extends LightningElement {
+
     cityName = ''
+    loadingText = ''
+    isError = false
+
+    get loadingClasses() {
+        return this.isError ? 'error-msg':'success-msg'
+    }
+
     searchHandler(event) {
         this.cityName = event.target.value
     }
@@ -13,7 +22,28 @@ export default class WeatherApp extends LightningElement {
     }
 
     fetchData() {
+        this.isError = false
+        this.loadingText = 'Fetching weather details...'
         console.log("cityName", this.cityName)
         // chamada da api
+
+        const URL = `https://api.openweathermap.org/data/2.5/weather?q=${this.cityName}&units=metric&appid=${API_KEY}`
+        fetch(URL).then(res=>res.json()).then(result=>{
+            console.log(JSON.stringify(result))
+           
+            this.weatherDetails(result)
+        }).catch((error)=> {
+            console.error(error)
+            this.loadingText = "something went wrong"
+            this.isError = true
+        })   
+    }
+    weatherDetails(info) {
+        if(info.cod === "404") {
+            this.isError = true
+            this.loadingText = `${this.cityName} isn't a valid city name` 
+        } else {
+            this.loadingText = ''
+        }
     }
 }
